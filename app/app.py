3dc8d748b52df08bf0a9cc91f378613d79373d49
Phuong Tran <phuongg.trann8799@gmail.com>
2020-10-22 00:04:56 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,7 +13,8 @@ def index():
 
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1,3)
-    loaded_model = pickle.load(open("./model.pkl",'rb'))
+    path = os.path.abspath("model.pkl")
+    loaded_model = pickle.load(open(path,'rb'))
     result = loaded_model.predict(to_predict)
     return result[0]
 
